088709956c582d6ef3463ca9de6ddcf6e31443da
mario32111 <marioge44@gmail.com>
2025-08-28 09:08:37 -0600

    feat: menu

---
+++ app/src/main/java/com/example/primerproyecto/Menu.kt
@@ -0,0 +1,31 @@
+package com.example.primerproyecto
+
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class Menu : AppCompatActivity() {
+    lateinit var button1: Button
+    lateinit var button2: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_menu)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        button1 = findViewById(R.id.button1)
+        button2 = findViewById(R.id.button2)
+
+        button1.setOnClickListener {
+
+        }
+    }
+}
